ad6a8dc05e5b2fd41a1ebb02b2696cb51d115b0f
marcelo <pasutmarcelo@gmail.com>
2020-01-22 00:18:27 -0300

    implemento on, off y toggle de dispositivos

--- app.js
+++ app.js
@@ -2,16 +2,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const ewelink = require('ewelink-api');
-
-const connection = new ewelink({
-    email: process.env.EWELINK_EMAIL,
-    password: process.env.EWELINK_PASS,
-    region: 'us'
-});
 
 /* get all devices */
-connection.getDevices().then(devices => console.log(devices));
+
 
 var indexRouter = require('./routes/index');
 
--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,19 @@ var Promise = require("bluebird");
 /* GET home page. */
 router.get('/devices', function(req, res, next) {
   Promise.resolve(devicesService.getDevices()).map(device => {
-    return {_id: device._id, name: device.name, state: device.params.switch, power: device.params.power,
+    return {_id: device.deviceid, name: device.name, state: device.params.switch, power: device.params.power,
       voltage: device.params.voltage, current: device.params.current, model: device.productModel}})
       .then(devices => res.status(200).send(devices));
+});
+
+router.patch('/devices/:id', function(req, res, next) {
+  const id = req.params.id;
+  const state = req.body.state;
+  devicesService.setDevicePowerState(id, state)
+      .then(status => {
+        if (status.status === 'ok') res.status(200).send(status.state);
+        else res.status(500).send();
+      });
 });
 
 module.exports = router;
